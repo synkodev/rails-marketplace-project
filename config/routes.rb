8f2504358ea49075b6d032f823e488af825a9d3a
cesar ramalho <cesar.ramalho@hotmail.com>
2023-08-30 15:24:27 -0300

    order-controller2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root to: "pages#home"
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  #resources :orders
+
+  resources :orders, only: [:index, :show, :edit]
+
 end
